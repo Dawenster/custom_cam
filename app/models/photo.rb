4b39cf212f7969245c04170f552be6ab49035fcb
Apprentice <apprentice@devbootcamp.com>
2013-04-05 12:04:29 -0700

    Hired FactoryGirl

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :body
+  belongs_to :user
+  belongs_to :contest
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+
+  factory :photo do
+    description "Super mega photo"
+    url "asdf"
+    contest_id 1
+    user_id 1
+  end
+end
