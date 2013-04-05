4807ad09f636e0039caf20bfa09affcfb01dc9ac
Apprentice <apprentice@devbootcamp.com>
2013-04-05 11:20:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinetyNinePhotoz::Application.config.secret_token = '6abb43612fdc3bee424dcb3210854bd693ddd71c93d6f92928c4ec17113c160392796388d16d9d05e124a093c38e735d4b0f3d712e5088534fafbda0861968e9'
