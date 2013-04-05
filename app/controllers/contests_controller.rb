d04f5fd58360f30bcd0039cf06d059f6d6a374fa
Apprentice <apprentice@devbootcamp.com>
2013-04-05 16:40:21 -0700

    Can create contests now

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -6,4 +6,23 @@ class ContestsController < ApplicationController
     @contest = Contest.find(params[:id])
     @photos = Photo.where('contest_id =?', params[:id])
   end
+
+  def new
+    @contest = Contest.new
+  end
+
+  def create
+    @contest = Contest.new(:title => params[:contest][:title],
+                           :description => params[:contest][:description],
+                           :price => params[:contest][:price].to_i,
+                           :category_id => 1, # TODO: THIS IS HARDCODED
+                           :creator_id => 1)
+
+    if @contest.save
+      redirect_to contest_path(@contest)
+    else
+      flash.now[:alert] = @contest.errors.full_messages.join(",")
+      render :new
+    end
+  end
 end
