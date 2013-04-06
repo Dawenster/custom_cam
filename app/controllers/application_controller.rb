1163e11d16a7055aebeadcdf8623a17883510a61
Apprentice <apprentice@devbootcamp.com>
2013-04-06 15:10:43 -0700

    rudimentary time left line on contest show page, no check for when duration ends

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,16 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user
+
+  def humanize(secs)
+    [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].inject([]){ |s, (count, name)|
+      if secs > 0
+        secs, n = secs.divmod(count)
+        s.unshift "#{n.to_i} #{name}"
+      end
+      s
+    }.join(' ')
+  end
+
+  helper_method :humanize
 end
