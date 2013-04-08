b7af67c7c30f3fac10930ae558817fa5be1303ba
Apprentice <apprentice@devbootcamp.com>
2013-04-07 18:40:50 -0700

    now it really works. wasted so much time for nothing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,19 +18,25 @@ class ApplicationController < ActionController::Base
   end
 
   def countdown_num(num)
-    if num.length > 5
+    puts num
+    if num.length > 6 && num.length < 9
       num.insert(0,"00:")
-    elsif num.length > 2
+      puts num
+    elsif num.length > 3 && num.length < 6
       num.insert(0,"00:00:")
-    else
+      puts num
+    elsif num.length > 0 && num.length < 3
       num.insert(0,"00:00:00:")
+      puts num
     end
 
+    puts num
     matches = num.match ( /(\d{0,2}):(\d{0,2}):(\d{0,2}):(\d{0,2})/ ) 
     correct_num = ""
     i = 1
     first = true
-    
+    puts matches.inspect
+
     ((matches.length) -1 ).times do
       if matches[i].length == 1 && first == false 
         correct_num << (":0" + matches[i] )
