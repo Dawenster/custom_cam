1cb37c808e113b4b0b6fc724a09dccf6a4b5d853
Dawenster <dstwen@gmail.com>
2013-04-08 14:41:54 -0700

    changed seed file to only have categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'faker'
+# require 'faker'
 
 categories = %w(Office Nature Family Animals Music Diversity Cities Holidays Sports Food Art Misc.)
 
@@ -6,25 +6,25 @@ categories.each do |category|
   Category.create(:title => category)
 end
 
-5.times do 
-  User.create(:username => Faker::Name.name,
-              :email => Faker::Internet.email,
-              :password => "Whatever",
-              :photographer => false)
-end
+# 5.times do 
+#   User.create(:username => Faker::Name.name,
+#               :email => Faker::Internet.email,
+#               :password => "Whatever",
+#               :photographer => false)
+# end
 
-20.times do
-  User.create(:username => Faker::Name.name,
-              :email => Faker::Internet.email,
-              :password => "Whatever",
-              :photographer => true)
-end
+# 20.times do
+#   User.create(:username => Faker::Name.name,
+#               :email => Faker::Internet.email,
+#               :password => "Whatever",
+#               :photographer => true)
+# end
 
-50.times do 
-  Contest.create(:title => Faker::Lorem.sentence,
-                 :description => Faker::Lorem.paragraph,
-                 :category_id => rand(1..12),
-                 :price => rand(50..500),
-                 :creator_id => rand(1..5),
-                 :duration => [3,5,7].sample)
-end
+# 50.times do 
+#   Contest.create(:title => Faker::Lorem.sentence,
+#                  :description => Faker::Lorem.paragraph,
+#                  :category_id => rand(1..12),
+#                  :price => rand(50..500),
+#                  :creator_id => rand(1..5),
+#                  :duration => [3,5,7].sample)
+# end
