94840caa7545e9520710c9978715c53ed5496374
Apprentice <apprentice@devbootcamp.com>
2013-04-07 17:18:22 -0700

    Solve when there is a winner image but no ratings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
   def user_photo_rating(photo)
-    current_user.ratings.select { |rating| rating.photo == photo } if current_user
+    if current_user
+      return nil if current_user.ratings.select { |rating| rating.photo == photo }.empty?
+      current_user.ratings.select { |rating| rating.photo == photo }
+    end
   end
 end
